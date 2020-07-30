c85f337aac2fe6c75e37125d43b64ce80f81f013
Marcelo Gonçalves <goncalves.marceloaugusto@gmail.com>
2020-07-30 08:40:44 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -20,87 +23,98 @@ import model.services.DepartmentService;
 public class DepartmentFormController implements Initializable {
 
 	private DepartmentService service;
-	
+
 	private Department entity;
-	
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
 	@FXML
 	private TextField txtId;
-	
-	
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
+
 	public void setDepartmentSer(DepartmentService service) {
 		this.service = service;
 
 	}
-	
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+
+	}
+
 	@FXML
 	private void onBtSaveAction(ActionEvent event) {
-	if (entity == null) {
-		throw new IllegalStateException("Entity was null");
-	}
-		
-	if (service == null) {
-		throw new IllegalStateException("Service was null");
-	}
-	try {
-		entity = getFormData(); 
-		service.saveOrUpdate(entity);
-		Utils.currentStage(event).close();	
-	}
-	catch (DbException e) {
-		Alerts.showAlert("Erro save object", null, e.getMessage(),AlertType.ERROR);
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
 		}
-	
+
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Erro save object", null, e.getMessage(), AlertType.ERROR);
+		}
+
 	}
-	
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
 		
+	}
+
 	private Department getFormData() {
-	    Department obj = new Department();
-	    obj.setId(Utils.tryParseToInt(txtId.getText()));
-	    obj.setName(txtName.getText());
-		
-	    return obj;
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+
+		return obj;
 	}
 
 	@FXML
 	private void onBtCanceAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		initializeNodes();		
+		initializeNodes();
 	}
-   
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	 public void updateFormData() {
-		 if (entity == null) {
-			 throw new IllegalStateException("Entity was null");
-		 }
-		 txtId.setText(String.valueOf(entity.getId()));
-		 txtName.setText(String.valueOf(entity.getName()));
-		 
-	 }
-	
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(String.valueOf(entity.getName()));
+
+	}
+
 }
-
-
